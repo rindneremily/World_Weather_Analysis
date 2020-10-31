380dbf798a5ca6492ab1e9518737607cb6ad395e
Emily Rindner <Emily@MacBook-Pro-9.local>
2020-10-31 14:25:07 -0700

    6.5.1

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 weather_api_key="d4cb209770713b56aa45c40025b8cc28"
+# from config import api_key, g_key
+g_key = "847ea107-56af-41a8-af10-c4a43d4004e5"
---
+++ config2.py
@@ -0,0 +1,2 @@
+from config import api_key, g_key
+g_key = "847ea107-56af-41a8-af10-c4a43d4004e5"
